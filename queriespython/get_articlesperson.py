a5d2be4f937c4f01a17f4ec74446605e3224a2e8
maracomputer <65308872+maracomputer@users.noreply.github.com>
2020-06-16 20:40:02 +0200

    Update get_articlesperson.py

--- queriespython/get_articlesperson.py
+++ queriespython/get_articlesperson.py
@@ -1,13 +1,12 @@
 # pip install sparqlwrapper
 # https://rdflib.github.io/sparqlwrapper/
 
-import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
 def get_results(endpoint_url, query):
-    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    user_agent = "WDQS-example Python/3.7" % (sys.version_info[0], sys.version_info[1])
     # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
     sparql.setQuery(query)
@@ -19,7 +18,6 @@ def get_results(endpoint_url, query):
 def get_articlesperson(endpoint_url, person):
     query =  """PREFIX schema: <http://schema.org/>
 PREFIX zbwext: <http://zbw.eu/namespaces/zbw-extensions/>
-#
 select distinct ?pm20 ?viewer ?workCount
 where {{
   # get the basic set of persons with "field of activity"
