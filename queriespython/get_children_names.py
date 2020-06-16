213146d84f1f7e54f65926fde105db447047ae13
Janine <65309615+js345-mini@users.noreply.github.com>
2020-06-16 14:32:26 +0200

    Create get_children_names.py

---
+++ queriespython/get_children_names.py
@@ -0,0 +1,31 @@
+# pip install sparqlwrapper
+# https://rdflib.github.io/sparqlwrapper/
+
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    print(sparql.query().convert())
+    return sparql.query().convert()
+
+
+def get_names_children(endpoint_url, person):
+    query = """SELECT ?child ?childLabel 
+WHERE
+{{
+  wd:{person} wdt:P40 ?child .
+SERVICE wikibase:label {{bd:serviceParam wikibase:language "en" }}
+}}
+
+
+""".format(person=person)
+    return(get_results(endpoint_url, query))
+
+get_names_children(endpoint_url, "Q230238")
